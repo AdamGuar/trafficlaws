3f518b5a6c85ca5967e53a0836c55031fe5b0f08
AdamGuar <adam2hp@gmail.com>
2023-08-17 16:59:03 +0200

    Add front end app folder

---
+++ app/src/index.ts
@@ -0,0 +1,12 @@
+function init(): void {
+    const button = document.querySelector("#testButton");
+    button?.addEventListener("click", testSubmitHandler);
+  }
+
+  function testSubmitHandler(e: Event): void {
+    e.preventDefault();
+    const outputElement = document.querySelector('#output');
+    outputElement.textContent = 'Scripts work!';
+  }
+
+  init();
